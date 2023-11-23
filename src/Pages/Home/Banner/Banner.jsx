d9106a9bd96fd1149a4dc221f48600b800239d09
sadid56 <sadidhasan56@gmail.com>
2023-11-23 22:15:13 +0600

    added a banner part

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -15,6 +15,7 @@ const Banner = () => {
             delay: 2000,
             disableOnInteraction: false,
           }}
+          loop={Infinity}
           effect={"fade"}
           modules={[Autoplay, EffectFade]}
           className="mySwiper relative">
@@ -28,14 +29,30 @@ const Banner = () => {
             <img src={slider3} className="max-h-screen w-full" alt="" />
           </SwiperSlide>
         </Swiper>
-        <div className="absolute top-1/4 z-10 w-3/5 pl-20 space-y-2">
-        <h2 className="text-6xl font-bold text-white uppercase">Build perfect body shape for good and healthy life.</h2>
-        <p className="text-white">Elevate your fitness game with our state-of-the-art Fitness Tracker! Seamlessly monitor steps, calories, and sleep quality. Achieve your wellness goals effortlessly. Your journey to a healthier you begins here!</p>
-        <button className="bg-[#fe1313] px-6 py-2 rounded-md font-medium text-white p-2 text-xl hover:bg-[#c20505]">Classes</button>
+        <div className="absolute top-1/4 z-10 w-3/5 ml-16 pl-5 space-y-2 border-l-2 border-[#fe1313]">
+          <div className="flex items-center gap-4">
+            <div className="border border-[#fe1313] w-[40px]"></div>
+            <p className="text-xl font-medium text-[#fe1313]">With Srong</p>
+          </div>
+          <h2 className="text-6xl font-bold text-white uppercase">
+            Build perfect body shape for good and healthy life.
+          </h2>
+          <p className="text-white">
+            Elevate your fitness game with our state-of-the-art Fitness Tracker!
+            Seamlessly monitor steps, calories, and sleep quality. Achieve your
+            wellness goals effortlessly. Your journey to a healthier you begins
+            here!
+          </p>
+          <button className="bg-[#fe1313] px-6 py-2 rounded-md font-medium text-white p-2 text-xl hover:bg-[#c20505]">
+            Classes
+          </button>
+        </div>
+        <img
+          className="absolute top-0 right-0 w-2/5 h-full z-10"
+          src={bgShap}
+          alt=""
+        />
       </div>
-      <img className="absolute top-0 right-0 w-2/5 h-full z-10" src={bgShap} alt="" />
-      </div>
-   
     </div>
   );
 };
